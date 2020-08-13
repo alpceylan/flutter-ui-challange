0fe51e75bce7ec7a93228ed025b4d5f6909daa4d
Alp Ceylan <alp.ceylan07@gmail.com>
2020-08-14 01:49:52 +0300

    week3 assets

--- week3_covidshop_ui/test/widget_test.dart
+++ week3_covidshop_ui/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:week3_covidshop_ui/main.dart';
+import 'package:covidShop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
