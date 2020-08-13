4dd60c1352bd33bbe49f6d7c8095eeec016ecd08
Alp Ceylan <alp.ceylan07@gmail.com>
2020-08-14 02:33:09 +0300

    product model

---
+++ week3_covidshop_ui/lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String title;
+  final String description;
+  final String imagePath;
+  final double price;
+  final String seller;
+
+  Product({
+    this.title,
+    this.description,
+    this.imagePath,
+    this.price,
+    this.seller,
+  });
+}
