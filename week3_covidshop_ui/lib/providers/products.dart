4bc3ce776b696638d55c0f2bf5699832186a0432
Alp Ceylan <alp.ceylan07@gmail.com>
2020-08-15 12:35:08 +0300

    week3 sanitization section completed

--- week3_covidshop_ui/lib/providers/products.dart
+++ week3_covidshop_ui/lib/providers/products.dart
@@ -3,70 +3,76 @@ import '../models/product.dart';
 
 class Products {
   List<Product> _products = [
-      Product(
-        title: 'Pro-Vac Venco',
-        description: 'A COVID-19 vaccine manufacturing plant of the institute in Kumming, capital city Yunnan Province, will be put into use in the second half of 2020',
-        imagePath: 'assets/images/pro-vac.png',
-        price: 34.50,
-        seller: 'johnsmith',
-        showcase: true,
-      ),
-      Product(
-        title: 'Live-B1 Hester',
-        description: 'It is a live, CEO vaccine wherein the virus strain is cultivated in SPF eggs. It has been lyophilized (freeze-dried) in vials and sealed under vacuum to preserve stability. It is supplied with diluent.The product meets rigid \'Quality Control\' standards of Purity, Safety and Potency as per individual monographs of viral vaccines of B.P',
-        imagePath: 'assets/images/live-b1.png',
-        price: 19.99,
-        seller: 'alpceylan',
-        showcase: true,
-      ),
-      Product(
-        title: 'Mask',
-        description: 'Made of KN95 grade particulates and new PP non-woven fabric. It filters %98 of particuls.',
-        imagePath: 'assets/images/mask.png',
-        price: 5.00,
-        seller: 'oliverbaker',
-        showcase: false,
-      ),
-      Product(
-        title: 'Soap',
-        description: 'Soap is a salt of a fatty acid[1] used in a variety of cleansing and lubricating products. In a domestic setting, soaps are surfactants usually used for washing, bathing, and other types of housekeeping. In industrial settings, soaps are used as thickeners, components of some lubricants, and precursors to catalysts.',
-        imagePath: 'assets/images/soap.png',
-        price: 2.99,
-        seller: 'harrybennet',
-        showcase: false,
-      ),
-      Product(
-        title: 'Gloves',
-        description: 'A glove is a garment covering the whole hand. Gloves usually have separate sheaths or openings for each finger and the thumb.',
-        imagePath: 'assets/images/gloves.png',
-        price: 10.99,
-        seller: 'mustafayilmaz',
-        showcase: false,
-      ),
-      Product(
-        title: 'Sanitizer',
-        description: 'Disinfectants are chemical agents designed to inactivate or destroy microorganisms on inert surfaces.[1] Disinfection does not necessarily kill all microorganisms, especially resistant bacterial spores; it is less effective than sterilization, which is an extreme physical or chemical process that kills all types of life.',
-        imagePath: 'assets/images/sanitizer.png',
-        price: 21.50,
-        seller: 'donaldthompson',
-        showcase: false,
-      ),
-    ];
+    Product(
+      title: 'Mask',
+      description:
+          'Made of KN95 grade particulates and new PP non-woven fabric. It filters %98 of particuls.',
+      imagePath: 'assets/images/mask.png',
+      price: 5.00,
+      seller: 'oliverbaker',
+      showcase: false,
+    ),
+    Product(
+      title: 'Gloves',
+      description:
+          'A glove is a garment covering the whole hand. Gloves usually have separate sheaths or openings for each finger and the thumb.',
+      imagePath: 'assets/images/gloves.png',
+      price: 10.99,
+      seller: 'mustafayilmaz',
+      showcase: false,
+    ),
+    Product(
+      title: 'Pro-Vac Venco',
+      description:
+          'A COVID-19 vaccine manufacturing plant of the institute in Kumming, capital city Yunnan Province, will be put into use in the second half of 2020',
+      imagePath: 'assets/images/pro-vac.png',
+      price: 34.50,
+      seller: 'johnsmith',
+      showcase: true,
+    ),
+    Product(
+      title: 'Live-B1 Hester',
+      description:
+          'It is a live, CEO vaccine wherein the virus strain is cultivated in SPF eggs. It has been lyophilized (freeze-dried) in vials and sealed under vacuum to preserve stability. It is supplied with diluent.The product meets rigid \'Quality Control\' standards of Purity, Safety and Potency as per individual monographs of viral vaccines of B.P',
+      imagePath: 'assets/images/live-b1.png',
+      price: 19.99,
+      seller: 'alpceylan',
+      showcase: true,
+    ),
+    Product(
+      title: 'Soap',
+      description:
+          'Soap is a salt of a fatty acid[1] used in a variety of cleansing and lubricating products. In a domestic setting, soaps are surfactants usually used for washing, bathing, and other types of housekeeping. In industrial settings, soaps are used as thickeners, components of some lubricants, and precursors to catalysts.',
+      imagePath: 'assets/images/soap.png',
+      price: 2.99,
+      seller: 'harrybennet',
+      showcase: false,
+    ),
+    Product(
+      title: 'Sanitizer',
+      description:
+          'Disinfectants are chemical agents designed to inactivate or destroy microorganisms on inert surfaces.[1] Disinfection does not necessarily kill all microorganisms, especially resistant bacterial spores; it is less effective than sterilization, which is an extreme physical or chemical process that kills all types of life.',
+      imagePath: 'assets/images/sanitizer.png',
+      price: 21.50,
+      seller: 'donaldthompson',
+      showcase: false,
+    ),
+  ];
 
-    final List<String> _productTypes = [
-      'Vaccine',
-      'Sanitizer',
-      'Mask',
-      'Gloves',
-      'Disinfectant',
-      'Hat',
-    ];
+  final List<String> _productTypes = [
+    'Vaccine',
+    'Sanitizer',
+    'Mask',
+    'Gloves',
+    'Disinfectant',
+    'Hat',
+  ];
 
-    List<Product> get products {
-      return [..._products];
-    }
+  List<Product> get products {
+    return [..._products];
+  }
 
-    List<String> get types {
-      return [..._productTypes];
-    }
+  List<String> get types {
+    return [..._productTypes];
+  }
 }
--- week3_covidshop_ui/lib/screens/home_screen.dart
+++ week3_covidshop_ui/lib/screens/home_screen.dart
@@ -21,6 +21,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double selectedIndex = 0;
+  bool showAll = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     final List<Widget> typeWidgets = [];
     final List<Widget> showcaseWidgets = [];
+    final List<Widget> sanitizationWidgets = [];
 
     types.forEach((type) {
       typeWidgets.add(
@@ -91,6 +93,162 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     });
 
+    if (!showAll) {
+      sanitizationWidgets.add(
+        Container(
+          height: deviceHeight * 0.3,
+          width: deviceWidth * 0.47,
+          child: Stack(
+            children: [
+              Container(
+                height: deviceHeight * 0.25,
+                width: deviceWidth * 0.44,
+                padding: EdgeInsets.symmetric(
+                  vertical: deviceHeight * 0.025,
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      products[0].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: deviceHeight * 0.06,
+                left: deviceWidth * 0.02,
+                child: Container(
+                  height: deviceHeight * 0.2,
+                  width: deviceWidth * 0.4,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(products[0].imagePath),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+      sanitizationWidgets.add(
+        Container(
+          height: deviceHeight * 0.3,
+          width: deviceWidth * 0.44,
+          child: Stack(
+            children: [
+              Container(
+                height: deviceHeight * 0.25,
+                width: deviceWidth * 0.48,
+                padding: EdgeInsets.symmetric(
+                  vertical: deviceHeight * 0.025,
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      products[1].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: deviceHeight * 0.06,
+                left: deviceWidth * 0.02,
+                child: Container(
+                  height: deviceHeight * 0.2,
+                  width: deviceWidth * 0.4,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(products[1].imagePath),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      products.forEach((product) {
+        sanitizationWidgets.add(
+          Container(
+          height: deviceHeight * 0.4,
+          width: deviceWidth * 0.47,
+          child: Stack(
+            children: [
+              Container(
+                height: deviceHeight * 0.25,
+                width: deviceWidth * 0.44,
+                padding: EdgeInsets.symmetric(
+                  vertical: deviceHeight * 0.025,
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      product.title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: deviceHeight * 0.06,
+                left: deviceWidth * 0.02,
+                child: Container(
+                  height: deviceHeight * 0.2,
+                  width: deviceWidth * 0.4,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(product.imagePath),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        );
+      });
+    }
+
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: PreferredSize(
@@ -103,31 +261,79 @@ class _HomeScreenState extends State<HomeScreen> {
           deviceHeight * 0.09,
         ),
       ),
-      body: Container(
-        height: deviceHeight,
-        width: deviceWidth,
-        padding: EdgeInsets.symmetric(
-          vertical: deviceHeight * 0.02,
-          horizontal: deviceWidth * 0.045,
-        ),
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [...typeWidgets],
+      body: SingleChildScrollView(
+        child: Container(
+          height: deviceHeight,
+          width: deviceWidth,
+          padding: EdgeInsets.symmetric(
+            vertical: deviceHeight * 0.02,
+            horizontal: deviceWidth * 0.045,
+          ),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [...typeWidgets],
+                ),
               ),
-            ),
-            SizedBox(
-              height: deviceHeight * 0.05,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [...showcaseWidgets],
+              SizedBox(
+                height: deviceHeight * 0.05,
               ),
-            ),
-          ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [...showcaseWidgets],
+                ),
+              ),
+              SizedBox(
+                height: deviceHeight * 0.06,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Sanitization',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showAll = !showAll;
+                      });
+                    },
+                    child: Text(
+                      showAll ? 'Close' : 'All',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: deviceHeight * 0.03,
+              ),
+              if (!showAll)
+                Row(
+                  children: [...sanitizationWidgets],
+                ),
+              if (showAll)
+                Expanded(
+                  child: GridView.count(
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: deviceWidth * 0.03,
+                    children: [...sanitizationWidgets],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
