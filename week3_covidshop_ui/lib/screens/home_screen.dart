d36c7b4bc30724468b32fa77666412f1da76aa0f
Alp Ceylan <alp.ceylan07@gmail.com>
2020-08-15 13:26:16 +0300

    week3 floating navigation bar

--- week3_covidshop_ui/lib/screens/home_screen.dart
+++ week3_covidshop_ui/lib/screens/home_screen.dart
@@ -22,6 +22,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double selectedIndex = 0;
+  double selectedScreen = 0;
   bool showAll = false;
 
   @override
@@ -209,6 +210,166 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
             ],
           ),
+        ),
+      ),
+      extendBody: true,
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(
+          left: deviceWidth * 0.16,
+          right: deviceWidth * 0.16,
+          bottom: deviceHeight * 0.04,
+        ),
+        padding: EdgeInsets.symmetric(
+          vertical: deviceHeight * 0.01,
+          horizontal: deviceWidth * 0.02,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.grey[800].withOpacity(0.8),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedScreen = 0;
+                });
+              },
+              child: Container(
+                height: deviceHeight * 0.055,
+                width: deviceWidth * 0.1,
+                decoration: BoxDecoration(
+                  color: selectedScreen == 0 ? Colors.amber : Colors.grey[500],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.home,
+                      color: selectedScreen == 0 ? Colors.black : Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedScreen = 1;
+                });
+              },
+              child: Container(
+                height: deviceHeight * 0.055,
+                width: deviceWidth * 0.1,
+                decoration: BoxDecoration(
+                  color: selectedScreen == 1 ? Colors.amber : Colors.grey[500],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.favorite_border,
+                      color: selectedScreen == 1 ? Colors.black : Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedScreen = 2;
+                });
+              },
+              child: Container(
+                height: deviceHeight * 0.055,
+                width: deviceWidth * 0.1,
+                decoration: BoxDecoration(
+                  color: selectedScreen == 2 ? Colors.amber : Colors.grey[500],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: selectedScreen == 2 ? Colors.black : Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedScreen = 3;
+                });
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    height: deviceHeight * 0.055,
+                    width: deviceWidth * 0.1,
+                    decoration: BoxDecoration(
+                      color:
+                          selectedScreen == 3 ? Colors.amber : Colors.grey[500],
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.shopping_basket,
+                          color:
+                              selectedScreen == 3 ? Colors.black : Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: Container(
+                      height: deviceHeight * 0.02,
+                      width: deviceWidth * 0.04,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color:
+                            selectedScreen == 3 ? Colors.black : Colors.amber,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(3),
+                        ),
+                      ),
+                      child: Text(
+                        '15',
+                        style: TextStyle(
+                          fontSize: 8,
+                          color:
+                              selectedScreen == 3 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
